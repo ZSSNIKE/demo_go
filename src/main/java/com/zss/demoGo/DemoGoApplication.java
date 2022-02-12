0f21c8358a0334517dd7d482da2afb130d7621ea
zhangss <2501207950@qq.com>
2022-02-12 14:06:41 +0800

    '第一次添加'

---
+++ src/main/java/com/zss/demoGo/DemoGoApplication.java
@@ -0,0 +1,13 @@
+package com.zss.demoGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGoApplication.class, args);
+    }
+
+}
